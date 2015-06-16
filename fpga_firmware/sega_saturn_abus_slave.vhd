8624c61b9d2b76dfbaeaa6c82ca944b52708372a
hitmo2500 <hitomi2500@mail.ru>
2015-06-16 18:21:31 +0300

    Preliminary Abus-to-avalon bridge

--- fpga_firmware/sega_saturn_abus_slave.vhd
+++ fpga_firmware/sega_saturn_abus_slave.vhd
@@ -39,9 +39,77 @@ entity sega_saturn_abus_slave is
 end entity sega_saturn_abus_slave;
 
 architecture rtl of sega_saturn_abus_slave is
+
+signal abus_address_buf         : std_logic_vector(25 downto 0) := (others => '0'); --          abus.address
+signal abus_writedata_buf       : std_logic_vector(15 downto 0) := (others => '0'); --              .data
+signal abus_chipselect_buf      : std_logic_vector(2 downto 0)  := (others => '0'); --              .chipselect
+signal abus_read_buf            : std_logic                     := '0';             --              .read
+signal abus_write_buf           : std_logic_vector(1 downto 0)  := (others => '0'); --              .write
+signal abus_functioncode_buf    : std_logic_vector(1 downto 0)  := (others => '0'); --              .functioncode
+signal abus_timing_buf          : std_logic_vector(2 downto 0)  := (others => '0'); --              .timing
+signal abus_addressstrobe_buf   : std_logic                     := '0';             --              .addressstrobe
+signal abus_interrupt_buf       : std_logic                     := '0';             --              .interrupt
+
+signal abus_read_buf2            : std_logic                     := '0';             --              .read
+signal abus_write_buf2           : std_logic_vector(1 downto 0)  := (others => '0'); --              .write
+
+
+signal avalon_readdata_latch    : std_logic_vector(15 downto 0) := (others => '0');
+
 begin
 
 	-- TODO: Auto-generated HDL template
+	
+	--abus transactions are async, so first we must latch incoming signals
+	--to get rid of metastability
+	process (clock)
+	begin
+		if rising_edge(clock) then
+			abus_address_buf <= abus_address;
+			abus_writedata_buf <= abus_address;
+			abus_chipselect_buf <= abus_address;
+			abus_read_buf <= abus_address;
+			abus_write_buf <= abus_address;
+			abus_functioncode_buf <= abus_address;
+			abus_timing_buf <= abus_address;
+			abus_addressstrobe_buf <= abus_address;
+			abus_interrupt_buf <= abus_address;
+		end if;
+	end process;
+	
+	--abus read/write latch
+	process (clock)
+	begin
+		if rising_edge(clock) then
+			abus_write_buf2 <= abus_write_buf;
+			abus_read_buf2 <= abus_read_buf;
+		end if;
+	end process;
+	
+	--abus write pulse is a rising??? edge if a write signal
+	abus_write_pulse <= abus_write_buf and not abus_write_buf2;
+	abus_read_pulse <= abus_read_buf and not abus_read_buf2;
+	
+	--avalon write logic
+	process (clock)
+	begin
+		if rising_edge(clock) then
+			if abus_write_pulse = '1' then
+				avalon_write <= '1';
+			elsif avalon_waitrequest='0' then
+				avalon_write <= '0';
+			end if;
+		end if;
+	end process;
+	
+	--avalon read logic
+	
+	
+	--avalon-to-abus mapping
+	avalon_address <= abus_address_buf;
+	avalon_writedata <= abus_writedata_buf;
+	abus_readdata_buf <= avalon_readdata;
+	
 
 	abus_waitrequest <= '0';
 
@@ -49,7 +117,7 @@ begin
 
 	avalon_read <= '0';
 
-	avalon_address <= "00000000000000000000000000";
+	
 
 	avalon_write <= '0';
 
