310230f26c098007ba26b14ee70159990b26613f
hitomi2500 <hitomi2500@mail.ru>
2021-06-08 00:10:11 +0300

    Descriptor update (does not work, BSODs the system, do not load)

--- mcu_blaster/USB_DEVICE/App/usbd_desc.c
+++ mcu_blaster/USB_DEVICE/App/usbd_desc.c
@@ -63,11 +63,11 @@
   * @{
   */
 
-#define USBD_VID     1155
+#define USBD_VID     0x09FB
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22336
-#define USBD_PRODUCT_STRING_FS     "STM32 Virtual ComPort"
+#define USBD_MANUFACTURER_STRING     "Altera"
+#define USBD_PID_FS     0x6001
+#define USBD_PRODUCT_STRING_FS     "USB-Blaster"
 #define USBD_CONFIGURATION_STRING_FS     "CDC Config"
 #define USBD_INTERFACE_STRING_FS     "CDC Interface"
 
