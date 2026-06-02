580b62e3a36b32bb35c0a0922120776652e2e4b8
hitomi2500 <kanzagi2500@gmail.com>
2026-06-02 09:25:14 +0300

    Directory flag

--- fpga_firmware_lattice/picorv_firmware/filesystem.c
+++ fpga_firmware_lattice/picorv_firmware/filesystem.c
@@ -295,7 +295,8 @@ int filesystem_access_scheduler() {
 				mini_printf("DELAY8\r\n");
 				if (FR_OK == f_stat(filename, &filinf)) {
 					int year = (filinf.fdate>>9)+1980;
-					mini_snprintf(reply_buffer,256,"OK name=\"%s\" size=\"%d\" date=\"%02d.%02d.%02d\"",filinf.fname,filinf.fsize,(filinf.fdate>>5)&0xf,(filinf.fdate)&0x1f,(year>=2000) ? year-2000 : year-1900);
+					int dir = (filinf.fattrib & AM_DIR) ? 1 : 0;
+					mini_snprintf(reply_buffer,256,"OK name=\"%s\" size=\"%d\" date=\"%02d.%02d.%02d\" dir=\"%d\"",filinf.fname,filinf.fsize,(filinf.fdate>>5)&0xf,(filinf.fdate)&0x1f,(year>=2000) ? year-2000 : year-1900, dir);
 				} else {
 					mini_snprintf(reply_buffer,256,"ERR File not found");
 				}
