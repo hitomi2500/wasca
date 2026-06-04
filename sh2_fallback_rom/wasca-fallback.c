924e4700762ebc9e03863c33ab8db9c911e909a7
hitomi2500 <kanzagi2500@gmail.com>
2026-06-04 13:39:45 +0300

    Bootstrap cleanup, fallback rom message update

--- sh2_fallback_rom/wasca-fallback.c
+++ sh2_fallback_rom/wasca-fallback.c
@@ -1,17 +1,17 @@
 #include <assert.h>
 #include <stdlib.h>
 
-char message[] = "Wasca : boot error.";
-int message_len = 19;//not using strlen to keep the binary size minimal
+char message[] = "wasca boot error";
 
 int
 start(void)
 {
-        int i;
+        int i=0;
         unsigned short * pVDP2_index = (unsigned short*)0x25e06208;
-        for (i=0;i<message_len; i++) {
+		while (message[i]!=0){
                 pVDP2_index[i] = message[i]<<1;
                 pVDP2_index[i+0x40] = (message[i]<<1) | 0x1;
+				i++;
         }
         while(1);
 }
