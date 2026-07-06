7c16f8d76d8619d5e1c819a8f2ade44a38b4bc11
hitomi2500 <kanzagi2500@gmail.com>
2026-07-06 10:06:34 +0300

    Pushing freq up to 145 MHz, additional pins power in lpf

--- fpga_firmware_lattice/hdl/pll.v
+++ fpga_firmware_lattice/hdl/pll.v
@@ -4,7 +4,7 @@
 // 6/17/17/3 = 50 / 141.68 mhz
 // 7/20/20/3 = 58.34 / 143.10 mhz
 // 8/23/23/4 = 50 / 143.75 mhz
-
+// 10/29/29/5 = 50 / 145 mhz
 
 module pll_25_133(input clk_in_25, output clk_out_50, output clk_out_133);
     (* ICP_CURRENT="6" *) (* LPF_RESISTOR="16" *) (* MFG_ENABLE_FILTEROPAMP="1" *) (* MFG_GMCREF_SEL="2" *)
@@ -18,10 +18,10 @@ module pll_25_133(input clk_in_25, output clk_out_50, output clk_out_133);
         .OUTDIVIDER_MUXA("DIVA"),
         .CLKOP_ENABLE("ENABLED"),
         .CLKOS_ENABLE("ENABLED"),
-        .CLKOP_DIV(8),
-        .CLKOS_DIV(23),
-        .CLKFB_DIV(23),
-        .CLKI_DIV(4),
+        .CLKOP_DIV(10),
+        .CLKOS_DIV(29),
+        .CLKFB_DIV(29),
+        .CLKI_DIV(5),
         .FEEDBK_PATH("CLKOP")
     ) pll_i (
         .CLKI(clk_in_25),
